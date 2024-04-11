6a4bfb0a709609263aa31bd60e8b1a9f2ede19f2
DhruvPrajapati21 <chetanbhai.dhruv@gmail.com>
2024-04-11 14:18:43 +0530

    my first commit'

--- lib/Notifications.dart
+++ lib/Notifications.dart
@@ -14,7 +14,7 @@ class _NotificationsState extends State<Notifications> {
         appBar: AppBar(
           backgroundColor: Colors.cyan,
           title: Text(
-            "Manage Notifications",
+            "Manag Notifications",
             style: TextStyle(
               fontStyle: FontStyle.italic,
               fontWeight: FontWeight.bold,
